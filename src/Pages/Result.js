c2a7a76b994f3ca278486260bc9fac7918e89cfb
lastcarol <lastcarol0315@gmail.com>
2023-03-04 22:27:23 +0900

    [6] Add Result Logic

--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -1,7 +1,78 @@
-import React from "react";
+import react from "react";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { Resultdata } from "../assets/data/resultdata";
 
 const Result = () => {
-  return <div>결과 화면입니다..</div>;
+  const navigate = useNavigate();
+
+  const handleClickRestart = () => {
+    navigate("/");
+  };
+
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기 </Header>
+      <Contents>
+        <Title>결과 보기</Title>
+        <LogoImage>
+          <img
+            src={Resultdata[0].image}
+            className="rounded-circle"
+            alt="..."
+            width={350}
+            height={350}
+          />
+        </LogoImage>
+        <Desc>
+          예비 집사님과 찰떡궁합인 고양이는 {Resultdata[0].name} 입니다.
+        </Desc>
+        <Button
+          style={{ fontFamily: "MaplestoryLight" }}
+          onClick={handleClickRestart}
+        >
+          테스트 다시하기
+        </Button>
+      </Contents>
+    </Wrapper>
+  );
 };
 
 export default Result;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "MaplestoryLight";
+`;
+
+const Contents = styled.div`
+  display: flex;
+  justify-contents: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+  font-family: "MaplestoryLight";
+`;
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`;
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+  font-family: "MaplestoryLight";
+`;
---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,131 @@
+import Abyssinian from "../cat/Abyssinian.jpg";
+import Ameshort from "../cat/Ameshort.jpg";
+import Bengal from "../cat/Bengal.jpg";
+import British from "../cat/British.jpg";
+import Exotic from "../cat/Exotic.jpg";
+import Korshort from "../cat/Korshort.jpg";
+import Mainecoon from "../cat/Mainecoon.jpg";
+import Munchikin from "../cat/Munchikin.jpg";
+import Norwegianforest from "../cat/Norwegianforest.jpg";
+import Persian from "../cat/Persian.jpg";
+import Ragdoll from "../cat/Ragdoll.jpg";
+import Russianblue from "../cat/Russianblue.jpg";
+import Scottishfold from "../cat/Scottishfold.jpg";
+import Siamese from "../cat/Siamese.jpg";
+import Sphinx from "../cat/Sphinx.jpg";
+import Turkishangora from "../cat/Turkishangora.jpg";
+
+export const Resultdata = [
+  {
+    id: 1,
+    name: "러시안 블루",
+    best: "ESTJ",
+    desc: "겁이 많아요",
+    image: `${Russianblue}`,
+  },
+  {
+    id: 2,
+    name: "샴",
+    best: "ESFP",
+    desc: "대체적으로 온순해요",
+    image: `${Siamese}`,
+  },
+  {
+    id: 3,
+    name: "아비시니안",
+    best: "ISTJ",
+    desc: "아비시안이에요",
+    image: `${Abyssinian}`,
+  },
+  {
+    id: 4,
+    name: "아메쇼트",
+    best: "ISTP",
+    desc: "아메쇼트에요",
+    image: `${Ameshort}`,
+  },
+  {
+    id: 5,
+    name: "뱅갈",
+    best: "INFJ",
+    desc: "뱅갈이에요",
+    image: `${Bengal}`,
+  },
+  {
+    id: 6,
+    name: "브리트쉬",
+    best: "INTJ",
+    desc: "브리트쉬이에요",
+    image: `${British}`,
+  },
+  {
+    id: 7,
+    name: "엑조틱",
+    best: "ISFJ",
+    desc: "엑조틱이에요",
+    image: `${Exotic}`,
+  },
+  {
+    id: 8,
+    name: "코르쇼트",
+    best: "ISFP",
+    desc: "코르쇼트에요",
+    image: `${Korshort}`,
+  },
+  {
+    id: 9,
+    name: "메이너칸",
+    best: "INFP",
+    desc: "메이너칸이에요",
+    image: `${Mainecoon}`,
+  },
+  {
+    id: 10,
+    name: "먼치킨",
+    best: "INTP",
+    desc: "먼치킨이에요",
+    image: `${Munchikin}`,
+  },
+  {
+    id: 11,
+    name: "노르웨잇간포레스트",
+    best: "ENFP",
+    desc: "노르웨이간포레스트에요",
+    image: `${Norwegianforest}`,
+  },
+  {
+    id: 12,
+    name: "페르시안",
+    best: "ENTP",
+    desc: "페르시안이에요",
+    image: `${Persian}`,
+  },
+  {
+    id: 13,
+    name: "레그돌",
+    best: "ESFJ",
+    desc: "레그돌이에요",
+    image: `${Ragdoll}`,
+  },
+  {
+    id: 14,
+    name: "스코티시 폴드",
+    best: "ESTP",
+    desc: "스코티시폴드에요",
+    image: `${Scottishfold}`,
+  },
+  {
+    id: 15,
+    name: "스핑크스",
+    best: "ENFJ",
+    desc: "스핑크스에요",
+    image: `${Sphinx}`,
+  },
+  {
+    id: 16,
+    name: "터키시 앙고라",
+    best: "ENTJ",
+    desc: "터키시 앙고라 에요",
+    image: `${Turkishangora}`,
+  },
+];
